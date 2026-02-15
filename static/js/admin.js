5f37c2223357e9168b3170c4b18723e7a72b5612
jhy <jhy@jhyun.net>
2026-02-15 22:55:10 +0800

    enhance message format

--- static/js/admin.js
+++ static/js/admin.js
@@ -84,14 +84,14 @@ function generateInviteMessage(name, url, ceremony) {
     var address = WEDDING_CONFIG.weddingAddress || '';
     var location = address ? venue + '\n' + address : venue;
     var ceremonyLine = ceremony
-        ? '🌿 ' + (WEDDING_CONFIG.ceremonyLabel || '草坪仪式') + '：' + WEDDING_CONFIG.ceremonyTime + '\n'
+        ? '🌿 ' + (WEDDING_CONFIG.ceremonyLabel || '草坪仪式') + '：' + WEDDING_CONFIG.weddingDate + ' ' + WEDDING_CONFIG.ceremonyTime + '\n'
         : '';
 
     return '致' + name + '，\n\n' +
         '诚挚邀请您参加' + WEDDING_CONFIG.groomName + ' & ' + WEDDING_CONFIG.brideName + '的婚礼！\n\n' +
         ceremonyLine +
-        '🗓 时间：' + WEDDING_CONFIG.weddingDate + '  ' + WEDDING_CONFIG.banquetTime + '\n' +
-        '📍 地点：' + location + '\n\n' +
+        '🗓 婚宴时间：' + WEDDING_CONFIG.weddingDate + '  ' + WEDDING_CONFIG.banquetTime + '\n' +
+        '📍 婚宴地点：' + location + '\n\n' +
         '这是为您准备的专属电子邀请函：\n' +
         url + '\n\n' +
         '❗️ 请在邀请函最后部份填写您是否出席以及出席人数，\n' +
